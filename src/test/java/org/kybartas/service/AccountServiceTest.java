f034e2f4e98eacb1b844bc4d6062ebb6547b8cf3
Kybartas <kristijonas78@gmail.com>
2025-05-03 19:14:21 +0300

    Add testing for getBalance method with and no date range and fix typo in testFilterByDateRange

--- src/test/java/org/kybartas/service/AccountServiceTest.java
+++ src/test/java/org/kybartas/service/AccountServiceTest.java
@@ -2,6 +2,7 @@ package org.kybartas.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.kybartas.entity.Account;
 import org.kybartas.entity.Statement;
 
 import java.math.BigDecimal;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AccountServiceTest {
 
     private AccountService accountService;
+    private Account dummyAccount1;
     private List<Statement> dummyStatements;
 
     @BeforeEach
@@ -24,14 +26,31 @@ public class AccountServiceTest {
                 new Statement("111", LocalDate.of(2025,2,2), "B", "things", new BigDecimal(20), "EUR", "K"),
                 new Statement("111", LocalDate.of(2025,3,3), "C", "things", new BigDecimal(30), "EUR", "D")
         );
+        dummyAccount1 = new Account("111", dummyStatements);
     }
 
     @Test
     void testFilterByDateRange() {
-        List<Statement> result = accountService.filterByDateRange(dummyStatements, LocalDate.of(2025,1,1), LocalDate.of(2025,2,2));
+        List<Statement> result = accountService.filterStatementsByDateRange(dummyStatements, LocalDate.of(2025,1,1), LocalDate.of(2025,2,2));
 
         assertEquals(2, result.size());
         assertEquals("A", result.get(0).getBeneficiary());
         assertEquals("B", result.get(1).getBeneficiary());
+    }
+
+    @Test
+    void testGetBalanceDateless() {
+        BigDecimal result = accountService.getBalance(dummyAccount1);
+
+        assertEquals(new BigDecimal(10), result);;
+    }
+
+    @Test
+    void testGetBalanceWithDateRange() {
+        BigDecimal result = accountService.getBalance(dummyAccount1, LocalDate.of(2025,1,1), LocalDate.of(2025, 2 ,2));
+        BigDecimal result1 = accountService.getBalance(dummyAccount1, LocalDate.of(2025,2,2), LocalDate.of(2025, 3,3));
+
+        assertEquals(new BigDecimal(40), result);
+        assertEquals(new BigDecimal(-10), result1);
     }
 }
