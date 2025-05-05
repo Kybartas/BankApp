378f6d7b4a1adf7bbb0eaf5d8c5066d2acb86c0e
Kybartas <kristijonas78@gmail.com>
2025-05-05 17:36:43 +0300

    Add comments to AccountService and improve style of dated exportCSV method

--- src/main/java/org/kybartas/service/AccountService.java
+++ src/main/java/org/kybartas/service/AccountService.java
@@ -17,6 +17,12 @@ import java.util.stream.Collectors;
 @Service
 public class AccountService {
 
+    /**
+     * Fills Account object with information from a CSV statements file
+     * @param filePath path of CSV file
+     * @return new Account object filled with information form CSV file
+     * @throws Exception in case CSVReader fails
+     */
     public Account importCSV (Path filePath) throws Exception {
         List<String[]> rawCSVData = CSVUtil.readRawCSV(filePath);
         List<String[]> filteredData = CSVUtil.filterSwedTable(rawCSVData);
@@ -24,6 +30,11 @@ public class AccountService {
         return new Account(statements.get(0).getAccountNumber(), statements);
     }
 
+    /**
+     * Generates a CSV bank statement for Account
+     * @param account Account object for statement generation
+     * @return byte array containing CSV statement data
+     */
     public byte[] exportCSV(Account account) {
 
         try {
@@ -52,17 +63,28 @@ public class AccountService {
         }
     }
 
+    /**
+     * Generates a CSV bank statement for Account for given date range
+     * @param account Account object for statement generation
+     * @param from start date
+     * @param to end date
+     * @return byte array containing CSV statement data
+     */
     public byte[] exportCSV(Account account, LocalDate from, LocalDate to) {
 
+        List<Statement> tempStatements = account.getStatements();
+
+        if (from != null && to != null) {
+            tempStatements = filterStatementsByDateRange(tempStatements, from, to);
+        }
+        
         try {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             CSVWriter writer = new CSVWriter(new OutputStreamWriter(out));
 
             writer.writeNext(new String[] {"Account Number", "Date", "Beneficiary", "Description", "Amount", "Currency", "Type"});
 
-            List<Statement> filteredStatements = filterStatementsByDateRange(account.getStatements(), from, to);
-
-            for (Statement s : filteredStatements) {
+            for (Statement s : tempStatements) {
                 writer.writeNext(new String[]{
                         s.getAccountNumber(),
                         s.getDate().toString(),
@@ -82,12 +104,25 @@ public class AccountService {
         }
     }
 
+    /**
+     * Modifies given statement List to be within bounds of given date range
+     * @param statements statements to filter
+     * @param from start date
+     * @param to end date
+     * @return modified list of statements
+     */
     public  List<Statement> filterStatementsByDateRange(List<Statement> statements, LocalDate from, LocalDate to) {
+
         return statements.stream()
                 .filter(s -> !s.getDate().isBefore(from) && !s.getDate().isAfter(to))
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Calculates balance of account by checking if transactions were K (credit) or D (debit).
+     * @param account account to calculate balance of
+     * @return balance of account
+     */
     public BigDecimal getBalance(Account account) {
 
         List<Statement> tempStatements = account.getStatements();
@@ -95,7 +130,6 @@ public class AccountService {
         BigDecimal balance = BigDecimal.ZERO;
 
         for (Statement statement : tempStatements) {
-
             if ("K".equals(statement.getType())) {
                 balance = balance.add(statement.getAmount());
             } else if ("D".equals(statement.getType())) {
@@ -106,6 +140,11 @@ public class AccountService {
         return balance;
     }
 
+    /**
+     * Calculates balance of account within date range by checking if transactions were K (credit) or D (debit).
+     * @param account account to calculate balance of
+     * @return balance of account for date range
+     */
     public BigDecimal getBalance(Account account, LocalDate from, LocalDate to) {
 
         List<Statement> tempStatements = account.getStatements();
@@ -127,5 +166,4 @@ public class AccountService {
 
         return balance;
     }
-
 }
