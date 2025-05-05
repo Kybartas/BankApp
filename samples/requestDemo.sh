6e41a0f7201a24f655393c54e9ddc5955ca5dc64
Kybartas <kristijonas78@gmail.com>
2025-05-05 19:42:47 +0300

    Add application demo to the project and improve README.md;2D

---
+++ samples/requestDemo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+printf "\n--- RUNNING APPLICATION ---\n\n"
+sleep 1
+java -jar ../target/juniorHomework-1.0.jar & SERVER_PID=$!
+
+sleep 6
+
+printf "\n--- IMPORTING STATEMENT1.CSV ---\n\n"
+sleep 1
+curl --location 'http://localhost:8080/api/statements/import' \
+--form 'file=@"statement1.csv"'
+
+sleep 2
+
+printf "\n--- IMPORTING STATEMENT2.CSV ---\n\n"
+sleep 1
+curl --location 'http://localhost:8080/api/statements/import' \
+--form 'file=@"statement2.csv"'
+
+sleep 2
+
+printf "\n--- GETTING BALANCE FOR  ACCOUNT LT444555666777888999 FROM 2024-01-01 TO 2024-01-10 ---\n\n"
+sleep 1
+curl --location 'http://localhost:8080/api/statements/getBalance?accountNumber=LT444555666777888999&from=2024-01-01&to=2024-01-10'
+printf "\n"
+
+sleep 2
+
+printf "\n--- GETTING BALANCE FOR LT222333444555666777 FROM 2024-01-01 TO 2024-01-10 ---\n\n"
+sleep 1
+curl --location 'http://localhost:8080/api/statements/getBalance?accountNumber=LT222333444555666777&from=2024-01-01&to=2024-01-10'
+printf "\n"
+
+sleep 2
+
+printf "\n--- EXPORTING STATEMENTS FROM 2024-01-01 TO 2024-01-10 ---\n\n"
+sleep 1
+curl --location 'http://localhost:8080/api/statements/export?from=2024-01-01&to=2024-01-10'
+
+sleep 2
+
+printf "\n--- STOPPING APPLICATION ---\n\n"
+sleep 1
+kill $SERVER_PID
